588c7de986e4330f3e1758114da7312b8167f8ae
Shubham Joshi <shubham.joshi2812@gmail.com>
2018-08-20 13:09:06 +0530

    task2

--- Calculator.php
+++ Calculator.php
@@ -17,5 +17,19 @@
             }
             echo $sum;
         }
+        
+        public function add($arguments){
+            $sum =0;
+            if(!empty($arguments[0])){
+                $params = explode(',', $arguments[0]);
+                if(!empty($params)){
+                    foreach ($params as $param) {
+                            $sum += $param;
+                    }				
+                }
+            }
+            echo $sum;
+        }
+        
     }
 ?>
